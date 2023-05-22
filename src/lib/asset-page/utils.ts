f1ba0cb953d7ea7e80abac161bc69390ae84bcc0
Haeven Kelley <haevenshane@gmail.com>
2023-05-22 10:40:15 -0500

    Cleanup legacy files

--- src/lib/asset-page/utils.ts
+++
@@ -1,5 +0,0 @@
-export const formatUSD = (value: string) => {
-  const numbersOnly = parseInt(value.replace(/[^\d]/g, ''));
-  if (!numbersOnly) return '';
-  return numbersOnly.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-};
